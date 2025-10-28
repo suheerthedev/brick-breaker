4bf8bc4ef92c8c2d0a33fe1d4fe8d96a9b85e409
suheerthedev <khansuheer1@gmail.com>
2025-10-28 22:03:41 +0500

    Created whole game

---
+++ lib/src/components/play_area.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:brick_breaker/src/brick_breaker.dart';
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flutter/rendering.dart';
+
+class PlayArea extends RectangleComponent with HasGameReference<BrickBreaker> {
+  PlayArea()
+    : super(
+        paint: Paint()..color = const Color(0xfff2e8cf),
+        children: [RectangleHitbox()],
+      );
+
+  @override
+  FutureOr<void> onLoad() async {
+    super.onLoad();
+    size = Vector2(game.width, game.height);
+  }
+}
